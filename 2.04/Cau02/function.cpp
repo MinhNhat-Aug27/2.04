c340f7c887021b58d285efc7cc0adf95f5d84efb
MinhNhat-Aug27 <81581691+MinhNhat-Aug27@users.noreply.github.com>
2021-04-08 13:30:29 +0700

    Update function.cpp

--- 2.04/Cau02/function.cpp
+++ 2.04/Cau02/function.cpp
@@ -54,15 +54,154 @@ float toDec(string X, int base)
 	return sum;
 }
 
-string singleP(string X)
+string toBin(string x) 
+//input : 1 so nguyen dang string
+//output : day bit tuong ung voi x
 {
-	return string();
+	long long int x_int = 0;
+	for (int i = 0; i < x.length(); i++)
+	{
+		x_int = x_int * 10 + getNum(x[i]);
+	}
+	string res = "";
+	do {
+		res = getChar(x_int % 2) + res;
+		x_int /= 2;
+	} while (x_int != 0);
+	return res;
 }
 
-string doubleP(string X)
+unsigned long long int binToDec(string input)
+//input : 1 chuoi nhi phan
+//output : 1 so thap phan tuong ung voi input
 {
-	return string();
+	int begin = 0;
+
+	unsigned long long int result = 0;
+	for (begin; begin < input.length(); begin++)
+	{
+		result += getNum(input[begin]) * pow(2, input.length() - begin - 1);
+	}
+	return result;
 }
+
+
+string DecimalToBase(unsigned long long int lli, int base)
+//input : so thap phan, va co so base can chuyen qua, o day la 16.
+//output : chuoi co so base tuong ung
+{
+	string res = "";
+	do {
+		long long int temp = lli % base;
+		lli /= base;
+		res = getChar(temp) + res;
+	} while (lli != 0);
+	return res;
+}
+
+string singleP(string x)
+//input : 1 so thuc can chuyen qua dau cham dong chinh xac don
+//output : dang chinh xac don tuong ung
+{
+	string kq = "";
+	int bitCount = 1;
+	int start = 0;
+	if (x[0] == '-')
+	{
+		kq = '1' + kq;
+		start = 1;
+	}
+	else {
+		kq = '0' + kq;
+	}
+
+	int dot_pos = 0;
+	for (dot_pos; dot_pos < x.length(); dot_pos++) 
+	{
+		if (x[dot_pos] == '.')
+			break;
+	}
+
+	string x_nguyen = "";
+	for (int i = start; i < dot_pos; i++)
+	{
+		x_nguyen = x_nguyen + x[i];
+	}
+	string x_nguyen_bin = toBin(x_nguyen);
+	bitCount += x_nguyen_bin.length();
+
+	float x_abs = toDec(x, 10);
+	int bit_rem = 24 - bitCount+1;
+	long long int temp = round(x_abs * pow(2, bit_rem));
+	string temp_str_bin = toBin(to_string(temp));
+	int exponent = 23 - bit_rem + 127;
+	string e_str = "";
+	while (exponent != 0)
+	{
+		e_str = getChar(exponent % 10) + e_str;
+		exponent /= 10;
+	}
+	string e_str_bin = toBin(e_str);
+	kq = kq + e_str_bin;
+	for (int j = 1; j < temp_str_bin.length(); j++)
+	{
+		kq = kq + temp_str_bin[j];
+	}
+	return DecimalToBase(binToDec(kq), 16);
+}
+
+string doubleP(string x)
+//input : 1 so thuc can chuyen qua dau cham dong chinh xac kep
+//output : dang chinh xac kep tuong ung 
+{
+	string kq = "";
+	int bitCount = 1;
+	int start = 0;
+	if (x[0] == '-')
+	{
+		kq = '1' + kq;
+		start = 1;
+	}
+	else {
+		kq = '0' + kq;
+	}
+
+	int dot_pos = 0;
+	for (dot_pos; dot_pos < x.length(); dot_pos++)
+	{
+		if (x[dot_pos] == '.')
+			break;
+	}
+
+	string x_nguyen = "";
+	for (int i = start; i < dot_pos; i++)
+	{
+		x_nguyen = x_nguyen + x[i];
+	}
+	string x_nguyen_bin = toBin(x_nguyen);
+	bitCount += x_nguyen_bin.length();
+	
+	float x_abs = toDec(x, 10);
+	int bit_rem = 53 - bitCount + 1;
+	long long int temp = round(x_abs * pow(2, bit_rem));
+	string temp_str_bin = toBin(to_string(temp));
+	int exponent = 52 - bit_rem +1023;
+	string e_str = "";
+	while (exponent != 0)
+	{
+		e_str = getChar(exponent % 10) + e_str;
+		exponent /= 10;
+	}
+	string e_str_bin = toBin(e_str);
+	kq = kq + e_str_bin;
+	for (int j = 1; j < temp_str_bin.length(); j++)
+	{
+		kq = kq + temp_str_bin[j];
+	}
+	return DecimalToBase(binToDec(kq), 16);
+}
+
+
 
 bool isInteger(string X)
 {
